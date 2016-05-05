478e8763b3c1ef009823988e1fda03c6d533e6f4
ssca <impetus_deep@ymail.com>
2016-05-05 21:19:32 +0200

    - aggiornata interfaccia e colori di Enomis Latex From Web : presto plugin online

--- background.js
+++ background.js
@@ -1,102 +1,140 @@
 // from classes.js: listen to copy the text via injected script
-$copy = new CopyToClipboard();
-$copy.backgroundListen();
+var _g = {
+    copy: new CopyToClipboard(),
+    store: new SettingsStorage(),
+    regex: {
+		wikipedia: new RegExp( '^https?:\/\/[^\/]+wikipedia.org' ,'i'),
+		wolfram: new RegExp('^https?:\/\/mathworld.wolfram.com', 'i')
+	}
+}
+
+// init copy listening
+_g.copy.backgroundListen();
 
 
-// azione all'aggiornamento di una pagina:
-// naturalmente posso svolgere degli if in base al tipo di caricamento
-chrome.tabs.onUpdated.addListener(function(id, info, tab){
-    // NB:
-    // questo script avvie sia quando info.status e' loading che quando e' complete!
-    // logs in background page
-    // console.log(id) // test
-    // console.log(info)
-    // console.log(tab)
-    
-
-
-
-    if( info.status === 'complete'){
-      
-      var match = false;
-      var url = tab['url'];
-      var chromeUrl = false;
-
-      if(typeof url === 'undefined') return;
-
-      var inj = ["lib/classes.js", "lib/managePage.js"];
-
-      // gli url come chrome-extension creano problemi di accesso
-      // visto che attualmente non mi servono, li ignoro
-      if(url.match( /^chrome.*:\/\//i ) ){
-        // console.log('url matchante: ' + url);
-        chromeUrl = true;
-        return;
-      }
-
-
-      if(url.match( /^https?:\/\/[^\/]+wikipedia.org/i )){
-        // NB: l'ordine di caricamento e' importante!
-        var def = inj.concat(["injection/wikipedia.js"]);
-        concatenateInjections(id, def);
-        match = true;
-      }
-
-      if(url.match( /^https?:\/\/mathworld.wolfram.com/i )){
-        // NB: l'ordine di caricamento e' importante!
-        var def = inj.concat(["injection/wolfram.js"]);
-        concatenateInjections(id, def);
-        match = true;
-      }
-
-      // update icon for matching tabs
-      if(match){
-        chrome.browserAction.setIcon({
-          path:  {
-              "19" : "images/19-aico.png",
-              "48" : "images/48-aico.png",
-              "128" : "images/128-aico.png"
-          },
-          tabId: id
-        });
-      }
-
-    }
-  
+// retry storage values (or defaults) and then start
+_g.store.getAll().always(function(r){
+    _g.settings = r;
+    init();
 });
 
 
+function init(){
+    // azione all'aggiornamento di una pagina:
+    chrome.tabs.onUpdated.addListener(tabs_check);
+}
 
-// 
-// chrome.browserAction.enable(id);
+
+// when settings are updated reload matching tabs to apply changes
+_g.store.onChange(function(changes){
+	_g.settings = changes.newValue;
+	// now update all matching tabs
+	chrome.tabs.query({}, function(tabAr){
+		tabAr.map(function(tb){
+			var match = false;
+			tb.url = tb.url || '';
+			for(var j in _g.regex){	if( tb.url.match(_g.regex[j]) ) match = true; };
+			if(match) chrome.tabs.reload(tb.id);
+		});
+	});
+})
+
+// naturalmente posso svolgere degli if in base al tipo di caricamento
+function tabs_check(id, info, tab){
+	// questo script si avvia quando info.status e' loading che quando e' complete!
+	// logs in background page
+	// console.log(id) // test
+	// console.log(info)
+	// console.log(tab)
+    
+    // check if monitor or not the page
+    var monitor = _g.settings.monitor; 
+	
+	if( info.status === 'complete'){
+		
+		var match = false;
+		var url = tab['url'];
+		var chromeUrl = false;
+		// console.log(url)
+
+		if(typeof url === 'undefined') return;
+
+		var inj = ["lib/classes.js", "lib/managePage.js"];
+
+		// gli url come chrome-extension creano problemi di accesso
+		// visto che attualmente non mi servono, li ignoro
+		if(url.match( /^chrome.*:\/\//i ) ){
+			console.log('url matchante: ' + url);
+			chromeUrl = true;
+			return;
+		}
+
+		if( monitor.wikipedia && url.match( _g.regex.wikipedia )){
+			// NB: l'ordine di caricamento e' importante!
+			var def = inj.concat(["injection/wikipedia.js"]);
+			concatenateInjections(id, def);
+			match = true;
+		}
+
+		if( monitor.wolfram && url.match( _g.regex.wolfram )){
+		  	// NB: l'ordine di caricamento e' importante!
+			var def = inj.concat(["injection/wolfram.js"]);
+		 	concatenateInjections(id, def);
+            match = true;
+		}
+
+		// update icon for matching tabs
+		if(match){
+			chrome.browserAction.setIcon({
+				path:	{
+					"19" : "images/19-aico.png",
+					"38" : "images/38-aico.png"
+				},
+				tabId: id
+			});
+		}
+
+	}	
+}
+
+
+// on icon click open in new tab
 chrome.browserAction.onClicked.addListener(function(){
-  console.log('click')
-    var manifest = chrome.runtime.getManifest();
-    var optionsUri = chrome.extension.getURL(manifest['options_page']);
-    var win = window.open(optionsUri, '_blanck');
-    win.focus();
+	var manifest = chrome.runtime.getManifest();
+	var optionsUri = chrome.extension.getURL(manifest['options_page']);
+	var win = window.open(optionsUri, '_blanck');
+	win.focus();
+});
+
+
+// retrieve tab it to pass in injected code (managePage.js) - content script
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){ 
+    if(request.action && request.action === 'setBadgeText'){
+    	chrome.browserAction.setBadgeText({text: request.text, tabId: sender.tab.id});
+    	chrome.browserAction.setBadgeBackgroundColor({color: '#888873', tabId: sender.tab.id});
+    }
 });
 
 
 // inject script into tab recursively: the array "ar" order is the same as injection, so you've to warning about scripts dependency
 function concatenateInjections(id, ar, scrpt){
 
-  // add scrpt to ar so inject() will be simple
-  if( typeof scrpt !== 'undefined' ) ar = ar.concat([scrpt]);
+	// add scrpt to ar so inject() will be simple
+	if( typeof scrpt !== 'undefined' ) ar = ar.concat([scrpt]);
 
-  var i = ar.length;
-  var idx = 0 ;
-  
-  (function (){
-    var that = arguments.callee;
-    idx++;
-    if(idx <= i){
-      var f = ar[idx-1];
-      chrome.tabs.executeScript(id, { file: f , /*allFrames : true,*/ runAt: "document_end"}, function() {
-          that(idx);
-      });
-    }
-  })();
+	var i = ar.length;
+	var idx = 0 ;
+	
+	(function (){
+	var that = arguments.callee;
+	idx++;
+	if(idx <= i){
+		var f = ar[idx-1];
+		chrome.tabs.executeScript(id, { file: f , /*allFrames : true,*/ runAt: "document_end"}, function() {
+			that(idx);
+		});
+	}
+	})();
 
 }
 
